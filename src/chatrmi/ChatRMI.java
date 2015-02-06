c4c24a99dd2f85591ddf1e4a2d3559e53bc76747
xmaryposax <mary182x@gmail.com>
2015-02-05 21:23:12 -0600

    New

--- src/chatrmi/ChatRMI.java
+++ src/chatrmi/ChatRMI.java
@@ -16,7 +16,7 @@ public class ChatRMI {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //Comentario
+        //Comenta
         // TODO code application logic here
     }
     
