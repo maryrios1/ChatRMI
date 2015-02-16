b8d402a70ba0057352acdd1a9db35bb489f66138
lheredia <lheredia@LHEREDIA-LAP.oradev.oraclecorp.com>
2015-02-16 16:03:47 -0600

    Merge origin/master
    Conflicts:
    	nbproject/private/private.properties
    	nbproject/private/private.xml
    	src/chatrmi/ChatRMI.java
    	src/chatrmi/impl/RemoteImpl.java
    	src/chatrmi/impl/ServerRMI.java
    	src/chatrmi/remote/Constants.java
    	src/chatrmi/remote/Message.java
    	src/chatrmi/ui/ClientChat.form
    	src/chatrmi/ui/ClientChat.java

--- src/chatrmi/ChatRMI.java
+++ src/chatrmi/ChatRMI.java
@@ -39,7 +39,7 @@ public class ChatRMI {
         }if(args[1].equals("cliente")){
             LoginChat x =  new LoginChat();
             x.setVisible(true);
-          //  x =  new LoginChat();
+            //x =  new LoginChat();
            // x.setVisible(true);
         }
         // TODO code application logic here
