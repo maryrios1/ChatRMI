1b3cc62feebb8ea7987aa9601d4e5d4c97f6c20a
xmaryposax <mary182x@gmail.com>
2015-02-18 00:11:25 -0600

    Revert "classes"
    This reverts commit 022266c0e93325add844cb329992fd61ffd0e7ca.

--- src/chatrmi/ChatRMI.java
+++ src/chatrmi/ChatRMI.java
@@ -36,7 +36,9 @@ public class ChatRMI {
             }
         }if(args[1].equals("cliente")){
             LoginChat x =  new LoginChat();
-            x.setVisible(true);       
+            x.setVisible(true);
+            //x =  new LoginChat();
+           // x.setVisible(true);
         }
         // TODO code application logic here
     }
