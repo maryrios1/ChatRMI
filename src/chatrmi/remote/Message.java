6611ba79faba28dafa58d85b535033e27f444099
carcasti <carcasti@CARCASTI-LAP.oradev.oraclecorp.com>
2015-02-18 16:08:38 -0600

    metodo to String para mejor legiibilidad de los mensajes

--- src/chatrmi/remote/Message.java
+++ src/chatrmi/remote/Message.java
@@ -62,6 +62,11 @@ public class Message implements Serializable,Runnable{
     }
 
     @Override
+    public String toString() {
+        return "Message{" + "UserName=" + UserName + ", Message=" + Message + '}';
+    }
+
+    @Override
     public void run() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
